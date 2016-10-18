5854cca2b3936705050cd09e97f42ea22e376d92
Benedikt Maier <benedikt.maier@cern.ch>
2016-10-18 13:25:17 +0200

    PandaProd

---
+++ Objects/interface/PEvent.h
@@ -0,0 +1,35 @@
+#ifndef PANDA_PEVENTINFO
+#define PANDA_PEVENTINFO
+
+#include <TObject.h>
+#include <TClonesArray.h>
+
+
+namespace panda
+{
+  class PEvent : public TObject
+  {
+    public:
+      PEvent():
+        runNumber(0),
+        lumiNumber(0),
+        eventNumber(0),
+        isData(false),
+        npv(0),
+        mcWeight(-1)
+        {
+          metfilters = new std::vector<bool>;
+          tiggers = new std::vector<bool>;
+        }
+    ~PEvent(){ delete metfilters; delete tiggers; }
+    
+    int runNumber, lumiNumber;
+    ULong64_t eventNumber;
+    bool isData;
+    int npv;
+    float mcWeight;
+    std::vector<bool> *metfilters, *tiggers;
+    ClassDef(PEvent,1)
+  };
+}
+#endif
