6c01bf8b884d6c9fe79f0996340f8ab2ce4cded9
Siddharth Narayanan <sidn@mit.edu>
2017-01-31 23:32:31 +0100

    reduce tarball size

--- setuprel.sh
+++ setuprel.sh
@@ -12,11 +12,13 @@ git cms-merge-topic emanueledimarco:ecal_smear_fix_80X
 git cms-merge-topic ikrav:egm_id_80X_v2
 git clone -b egm_id_80X_v1 https://github.com/ikrav/RecoEgamma-ElectronIdentification.git RecoEgamma/ElectronIdentification/data.new
 rsync -avP RecoEgamma/ElectronIdentification/data.new/* RecoEgamma/ElectronIdentification/data/
+rm -rf  RecoEgamma/ElectronIdentification/data.new/
 
 # pho ID
 git cms-merge-topic ikrav:egm_id_80X_v3_photons
 git clone -b egm_id_80X_v1 https://github.com/ikrav/RecoEgamma-PhotonIdentification.git RecoEgamma/PhotonIdentification/data.new
 rsync -avP RecoEgamma/PhotonIdentification/data.new/* RecoEgamma/PhotonIdentification/data/
+rm -rf RecoEgamma/PhotonIdentification/data.new/
 
 # it's okay, at least our detector isn't on fire
 git cms-add-pkg EgammaAnalysis/ElectronTools
